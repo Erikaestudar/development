883df12a71da14ca751cc4b5c8f8b91239d090cc
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2024-11-12 20:49:49 -0300

    test

--- todo-app-main/teste01.js
+++ todo-app-main/teste01.js
@@ -1,6 +1,7 @@
-
+const body = document.body
+body.style.fontFamily = `"Josefin Sans", sans-serif`
+body.style.backgroundColor = 'hsl(0, 0%, 98%)'
 
 function change_theme() {
-    var body = document.body
     body.style.background = 'hsl(235, 21%, 11%)'
 }
