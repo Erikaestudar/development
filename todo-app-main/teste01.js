eddadcfd1a3d0a8883421c2145f3d53667cf980a
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2024-11-13 21:48:01 -0300

    create of the list with JS

--- todo-app-main/teste01.js
+++ todo-app-main/teste01.js
@@ -2,16 +2,33 @@
 document.body.style.fontFamily = `"Josefin Sans", sans-serif`
 document.body.style.backgroundColor = 'hsl(0, 0%, 98%)'
 
-const form = document.querySelector('input#itodo')
+const input = document.getElementById('ilist')
+const form = document.getElementById('taskForm')
 
 const list = document.querySelector('ul.list')
-form.addEventListener('keypress', enter)
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+})
+
+
+input.addEventListener('keyup', function(e) {if (e.key === 'Enter' && input.value.trim() !== '') {
+    const li = document.createElement('li')
+    li.classList.add(`list-item`)
+    li.textContent = input.value
+    li.style.fontSize = '1.2rem'
+    li.style.listStylePosition = 'inside'
+    li.style.marginBottom = '0.75rem'
+    li.style.marginLeft = '0.9rem'
+
+    list.appendChild(li)
+    input.value = ''
+}
+
+})
 //const line = document.createElement('li')
 
 function change_theme() {
     document.body.style.background = 'hsl(235, 21%, 11%)'
 
-    function enter() {
-        list.firstElementChild.textContent = form.value
-    }
 }
