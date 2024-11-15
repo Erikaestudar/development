203812f1780f0489a489765b88d222f2613058a0
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2024-11-15 12:57:17 -0300

    again the same day

--- todo-app-main/teste01.js
+++ todo-app-main/teste01.js
@@ -2,9 +2,9 @@
 document.body.style.fontFamily = `"Josefin Sans", sans-serif`
 document.body.style.backgroundColor = 'hsl(0, 0%, 98%)'
 
+// Seleção dos elementos
 const input = document.getElementById('ilist')
 const form = document.getElementById('taskForm')
-
 const list = document.querySelector('ul.list')
 
 form.addEventListener('submit', function(e) {
@@ -18,15 +18,21 @@ input.addEventListener('keyup', function(e) {if (e.key === 'Enter' && input.valu
     li.innerHTML = `<button id='btn-check' onclick='checked()'><svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#FFFFFF"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></button> ${input.value} <button id='btn-del' onclick='del()' title="Delet the task"><svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></button>`
     //li.textContent += input.value
 
-
     list.appendChild(li)
     input.value = ''
 }
 
 })
-//const line = document.createElement('li')
 
+    // function para salvar os dados e o layout
 function change_theme() {
     document.body.style.background = 'hsl(235, 21%, 11%)'
 
 }
+
+list.addEventListener('click', function(e) {
+    if (e.target.tagName === "li" || e.target.tagName === "button") {
+        e.target.classList.toggle("btn-checked")
+    }
+
+})
