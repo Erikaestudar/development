3633c6771cb5fdcc8687a35a48d1399ee95a639f
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2024-11-14 22:36:35 -0300

    button del

--- todo-app-main/teste01.js
+++ todo-app-main/teste01.js
@@ -15,7 +15,8 @@ form.addEventListener('submit', function(e) {
 input.addEventListener('keyup', function(e) {if (e.key === 'Enter' && input.value.trim() !== '') {
     const li = document.createElement('li')
     li.classList.add('list-item')
-    li.textContent = input.value
+    li.innerHTML = `<button id='btn-check' onclick='checked()'><svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#FFFFFF"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></button> ${input.value} <button id='btn-del' onclick='del()'><svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></button>`
+    //li.textContent += input.value
 
 
     list.appendChild(li)
